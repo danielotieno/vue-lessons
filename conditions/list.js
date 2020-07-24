5d290c4ba2291f29364f41ee99785e2c387853a6
danielotieno <danielotieno.ke@gmail.com>
2020-07-24 13:08:20 +0300

    chore:learning conditions and list

---
+++ conditions/list.js
@@ -0,0 +1,18 @@
+new Vue({
+  el: '#app',
+  data: {
+    ingredients: ['meat', 'fruits', 'cookies'],
+    persons: [
+      {
+        name: 'Daniel',
+        age: 20,
+        color: 'grey',
+      },
+      {
+        name: 'Rita',
+        age: 25,
+        color: 'blue',
+      },
+    ],
+  },
+});
